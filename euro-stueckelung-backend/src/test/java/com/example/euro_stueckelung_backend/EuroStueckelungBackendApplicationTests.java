1ad55b3714d9f4a0589e00e6993b507d0156a018
Ali-Uen <ali.uenal001@gmail.com>
2025-10-22 13:58:58 +0200

    chore: scaffold spring boot backend

---
+++ euro-stueckelung-backend/src/test/java/com/example/euro_stueckelung_backend/EuroStueckelungBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euro_stueckelung_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroStueckelungBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
